ff0691619e46b3fa24c9e22f3355a976e8a17e4d
mishravivek <mishravivek@microsoft.com>
2023-03-15 15:37:38 -0500

    push ;

--- .scripts/setup-env-variables-azure-template.sh
+++ .scripts/setup-env-variables-azure-template.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # ==== Resource Group ====
-export SUBSCRIPTION=subscription-id # customize this
-export RESOURCE_GROUP=resource-group-name # customize this
-export REGION=westus2
+export SUBSCRIPTION= # customize this
+export RESOURCE_GROUP=autoscalling # customize this
+export REGION=eastus
 
 # ==== Service and App Instances ====
-export SPRING_CLOUD_SERVICE=azure-spring-cloud-name # customize this
+export SPRING_CLOUD_SERVICE=petclinic-autoscaling # customize this
 export API_GATEWAY=api-gateway
 export ADMIN_SERVER=admin-server
 export CUSTOMERS_SERVICE=customers-service
@@ -21,15 +21,15 @@ export VETS_SERVICE_JAR=spring-petclinic-vets-service/target/spring-petclinic-ve
 export VISITS_SERVICE_JAR=spring-petclinic-visits-service/target/spring-petclinic-visits-service-2.5.1.jar
 
 # ==== MYSQL INFO ====
-export MYSQL_SERVER_NAME=mysql-servername # customize this
+export MYSQL_SERVER_NAME=autoscaling-mysql # customize this
 export MYSQL_SERVER_FULL_NAME=${MYSQL_SERVER_NAME}.mysql.database.azure.com
-export MYSQL_SERVER_ADMIN_NAME=admin-name # customize this
+export MYSQL_SERVER_ADMIN_NAME=apetclinicadmin # customize this
 export MYSQL_SERVER_ADMIN_LOGIN_NAME=${MYSQL_SERVER_ADMIN_NAME}\@${MYSQL_SERVER_NAME}
 export MYSQL_SERVER_ADMIN_PASSWORD=SuperS3cr3t # customize this
 export MYSQL_DATABASE_NAME=petclinic
 
 # ==== KEY VAULT Info ====
-export KEY_VAULT=your-keyvault-name # customize this
+export KEY_VAULT=petclinickey # customize this
 
 # ==== LOG ANALYTICS Info ====
 export LOG_ANALYTICS=${SPRING_CLOUD_SERVICE}
